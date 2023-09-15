f592465c67a76a1d34799dbbdec1572bf523d627
Cheuk Lam Cheung <ccheung792423@gmail.com>
2023-09-14 22:23:17 -0400

    Added images, captions, text content, table, button on new page

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.querySelector("#praise").addEventListener("click", ()=>{
-    document.querySelector("#happy").append(":D");
-})
+let praiseButton = document.querySelector("#praise")
+if (praiseButton){
+    praiseButton.addEventListener("click", ()=>{
+        document.querySelector("#happy").append(":D");
+    })
+}
+
+let dogButton = document.querySelector("#dog")
+if (dogButton){
+    dogButton.addEventListener("click", ()=> {
+        const dogWidth = document.querySelector("#friendly").offsetWidth * 1.5
+        document.getElementById("friendly").style.width = `${dogWidth}px`
+    })
+}
